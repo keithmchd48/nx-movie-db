1ab0f46a1276592fa954d15fcce64dc4c98ed86e
keithmchd48 <keithmchd48@gmail.com>
2024-03-23 11:14:06 +0100

    tailwind

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const route = useRoute();
+</script>
+
+<template>
+  <div>
+    <h1 class="text">Home</h1>
+    <p>Full path: {{ route.fullPath }}</p>
+  </div>
+</template>
